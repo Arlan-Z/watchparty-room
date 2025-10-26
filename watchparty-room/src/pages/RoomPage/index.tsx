d04ea4eef8ae59fea8df82c0509d135c2b36398f
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-10-26 11:59:42 +0500

    Added loading component, Added roomId validation

--- watchparty-room/src/pages/RoomPage/index.tsx
+++ watchparty-room/src/pages/RoomPage/index.tsx
@@ -3,23 +3,53 @@ import Chat from "./components/Chat";
 import Video from "./components/Video";
 import './RoomPage.css';
 import PlaceholderPage from "../PlaceholderPage";
+import { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
 
 export default function RoomPage() {
-   const { roomId } = useParams();
-   if (!roomId) {
-    return <PlaceholderPage placeholderText="Room not Found"/>
-   }
+  const { roomId } = useParams();
+  const [roomExists, setRoomExists] = useState<boolean | null>(null);
+
+   
+  useEffect(() => {
+    if (!roomId) {
+      setRoomExists(false);
+      return;
+    }
+
+    fetch(`/stream-service/api/rooms/${roomId}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Room not found");
+        return res.json();
+      })
+      .then((data) => {
+        if (data && data.id) {
+          setRoomExists(true);
+        } else {
+          setRoomExists(false);
+        }
+      })
+      .catch(() => setRoomExists(false));
+  }, [roomId]);
+
+  if (roomExists === null) {
+    return <Loading/>
+  }
+
+  if (!roomExists) {
+    return <PlaceholderPage placeholderText="Room not found" />;
+  }
 
   return (
     <div className="room-page-wrapper">
       <div className="video-box">
         <div className="video-wrapper">
-          <Video roomId={roomId}/>
+          <Video roomId={roomId!}/>
         </div>
       </div>
 
       <div className="chat-box">
-        <Chat roomId = {roomId}/>
+        <Chat roomId = {roomId!}/>
       </div>
     </div>
   );
